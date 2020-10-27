6e87bc8c3128382275686e07ed96aad1ecb22ded
Param Chauhan <paramchauhan21@gmail.com>
2020-10-27 22:21:31 +0530

    Add basic logic and endpoints

---
+++ shortner.py
@@ -0,0 +1,47 @@
+import uuid
+import redis
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
+
+# Start app and server
+app = FastAPI()
+r = redis.Redis()
+
+
+# Class to store url-name pair
+class Item(BaseModel):
+    url: str
+    custom_target:str = None
+
+
+# Test the root
+@app.get("/")
+def read_root():
+    return {"message": "Yo Yo Param Singh!"}
+
+
+# Redirect
+@app.get("/{short}")
+def redirect_url(short: str):
+    for key in r.keys():
+        if r.get(key).decode("utf8") == short:
+            return RedirectResponse(url=key.decode("utf8"))
+
+    return {"message": "URL not defined"}
+
+
+# Define POST
+@app.post("/")
+def shorten_url(item: Item):
+
+    url = item.url
+
+    if r.get(url) is None:
+        new_name = item.custom_target or str(uuid.uuid4())[-6:]
+        if r.mset({url: new_name}):
+            return {"url": url, "short": r.get(url)}
+        else:
+            return {"message": "failed"}
+
+    return {"message": "URL already exists", "short": r.get(url)}
